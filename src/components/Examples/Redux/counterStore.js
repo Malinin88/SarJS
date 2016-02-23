1af39953aa759a4b78f2a2dc8e6b9ffc7c4138c5
Pavel <spl.novikov@gmail.com>
2016-02-23 17:30:15 +0300

    added to examples custom create store function

--- src/components/Examples/Redux/counterStore.js
+++ src/components/Examples/Redux/counterStore.js
@@ -17,4 +17,30 @@ const counterStore = createStore(counterReducer);
 // (registers a function to be called on state changes)
 // 4. replaceReducer(nextReducer) can be used to implement hot reloading and code splitting. Most likely you won’t use it.
 
+// How to implement store from the scratch:
+/*
+const customCreateStore = (reducer) => {
+    let state;
+    let listeners = [];
+    const getState = () => state;
+
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener());
+    };
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => l !== listener);
+        };
+    };
+
+    // for initial state populated:
+    dispatch({});
+
+    return { getState, dispatch, subscribe };
+};
+*/
+
 module.exports = counterStore;
