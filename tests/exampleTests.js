419f8ff5aafd1b9751b41ddd33db2b14d9b11ad2
Pavel <spl.novikov@gmail.com>
2016-02-25 11:32:18 +0300

    added array mutation avoidance example

--- tests/exampleTests.js
+++ tests/exampleTests.js
@@ -5,3 +5,4 @@
 // [Novikov] todo: do we really have not another approach to start all tests instead of require?
 require('../src/components/Examples/Redux/counterReducerTests');
 require('../src/components/Examples/Redux/counterStoreTests');
+require('../src/components/Examples/AvoidMutations/arrayMutationsTest');
